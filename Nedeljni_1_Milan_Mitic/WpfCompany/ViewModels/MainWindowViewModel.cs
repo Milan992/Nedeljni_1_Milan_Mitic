17edcd3277bdae578ccb52d2ff6ded7db739edfc
Milan992 <mitic.gsm@gmail.com>
2020-08-05 22:20:44 +0200

    Created LogIn window

--- Nedeljni_1_Milan_Mitic/WpfCompany/ViewModels/MainWindowViewModel.cs
+++ Nedeljni_1_Milan_Mitic/WpfCompany/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
+using System.Windows.Input;
 
 namespace WpfCompany.ViewModels
 {
     class MainWindowViewModel : ViewModelBase
     {
+        Service service = new Service();
+        MainWindow main;
+
+        #region Constructors
+
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private string userName;
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand logIn;
+
+        public ICommand LogIn
+        {
+            get
+            {
+                if (logIn == null)
+                {
+                    logIn = new RelayCommand(param => LogInExecute(), param => CanLogInExecute());
+                }
+
+                return logIn;
+            }
+        }
+
+        private void LogInExecute()
+        {
+            try
+            {
+               
+            }
+            catch
+            {
+                MessageBox.Show("");
+            }
+        }
+
+        private bool CanLogInExecute()
+        {
+            return true;
+        }
+
+        #endregion
     }
 }
